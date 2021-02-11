14dd600623b674b4aa3d92447ccbe507ad405a92
Medardo Paz <medpazord2010@gmail.com>
2021-02-11 18:39:17 -0500

    primera clase

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  //template: `<p>Hola</p>`,
+  styleUrls: ['./empleado.component.css']
+  //styles:["p{background-color: green;}"]
+})
+export class EmpleadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
